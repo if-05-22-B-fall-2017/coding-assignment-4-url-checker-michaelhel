e902aa289ede4c45161922040964429300c0084c
michaelhel <michael.helcig@mailbox.org>
2018-04-24 14:13:25 +0200

    Added Pattern and Matcher

--- src/main/java/HttpUrlChecker.java
+++ src/main/java/HttpUrlChecker.java
@@ -1,6 +1,6 @@
 /*
- * Copyright ©2018. Created by P. Bauer (p.bauer@htl-leonding.ac.at),
- * Department of Informatics and Media Technique, HTBLA Leonding,
+ * Copyright ©2016. Created by P. Bauer (p.bauer@htl-leonding.ac.at),
+ * Department of Informatics and Media Technique, HTBLA Leonding, 
  * Limesstr. 12 - 14, 4060 Leonding, AUSTRIA. All Rights Reserved. Permission
  * to use, copy, modify, and distribute this software and its documentation
  * for educational, research, and not-for-profit purposes, without fee and
@@ -9,21 +9,39 @@
  * appear in all copies, modifications, and distributions. Contact the Head of
  * Informatics and Media Technique, HTBLA Leonding, Limesstr. 12 - 14,
  * 4060 Leonding, Austria, for commercial licensing opportunities.
- *
+ * 
  * IN NO EVENT SHALL HTBLA LEONDING BE LIABLE TO ANY PARTY FOR DIRECT,
  * INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES, INCLUDING LOST
  * PROFITS, ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION,
- * EVEN IF HTBLA LEONDING HAS BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- *
+ * EVEN IF HTBLA LEONDING HAS BEEN ADVISaED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * 
  * HTBLA LEONDING SPECIFICALLY DISCLAIMS ANY WARRANTIES, INCLUDING, BUT NOT
  * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
  * PARTICULAR PURPOSE. THE SOFTWARE AND ACCOMPANYING DOCUMENTATION, IF ANY,
  * PROVIDED HEREUNDER IS PROVIDED "AS IS". HTBLA LEONDING HAS NO OBLIGATION
  * TO PROVIDE MAINTENANCE, SUPPORT, UPDATES, ENHANCEMENTS, OR MODIFICATIONS.
  */
+package httpurl;
+
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+/**
+ *
+ * @author P. Bauer (p.bauer@htl-leonding.ac.at)
+ */
 public class HttpUrlChecker {
-
+        
+        public static Matcher getMatcher(String text) {
+            Pattern pattern = Pattern.compile("^http(s)?:\\/\\/"
+                    + "(www.)?"
+                    + "(\\w)+"
+                    + "(-(\\w)+)?"
+                    + "(\\.[a-zA-Z][a-zA-Z]+)+$");
+            
+            return pattern.matcher(text);
+            
+            
+        }
 }
+
